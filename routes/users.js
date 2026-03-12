34b673fb2245321b2a8022d1cdce0d068dca3692
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-03-12 15:50:58 +0700

    Validate

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+let { body, validationResult } = require('express-validator')
 
 let userModel = require("../schemas/users");
 
@@ -25,29 +26,57 @@ router.get("/:id", async function (req, res, next) {
   }
 });
 
-router.post("/", async function (req, res, next) {
-  try {
-    let newItem = new userModel({
-      username: req.body.username,
-      password: req.body.password, // không mã hoá
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      status: req.body.status,
-      role: req.body.role,
-      loginCount: req.body.loginCount
-    });
-
-    await newItem.save();
-
-    // populate cho đẹp
-    let saved = await userModel
-      .findById(newItem._id)
-    res.send(saved);
-  } catch (err) {
-    res.status(400).send({ message: err.message });
+router.post("/", [
+  body("email").notEmpty().withMessage("email khong duoc de trong").bail().isEmail().withMessage("email sai dinh dang"),
+  body("username").notEmpty().withMessage("username khong duoc de trong").bail().isAlphanumeric().withMessage("username khong duoc chua ki tu dac biet"),
+  body("password").notEmpty().withMessage("password khong duoc de trong").bail().isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1
+  }).withMessage("password dai it nhat 8 ki tu, trong do co it nhat 1 ki tu hoa, 1 ki tu thuong, 1 ki tu so va 1 ki tu dac biet"),
+  body("role").notEmpty().withMessage("role khong duoc de trong").bail().isMongoId().withMessage("role phai la 1 id"),
+  body("avatarUrl").optional().isArray().withMessage("image khong hop le"),
+  body("avatarUrl.*").optional().isURL().withMessage("Url khong hop le")
+],
+  function (req, res, next) {
+    let result = validationResult(req);
+    if (result.errors.length > 0) {
+      res.status(404).send(result.errors.map(
+        function (e) {
+          return {
+            [e.path]: e.msg
+          }
+        }
+      ))
+    } else {
+      next()
+    }
   }
-});
+  , async function (req, res, next) {
+    try {
+      let newItem = new userModel({
+        username: req.body.username,
+        password: req.body.password, // không mã hoá
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        status: req.body.status,
+        role: req.body.role,
+        loginCount: req.body.loginCount
+      });
+
+      await newItem.save();
+
+      // populate cho đẹp
+      let saved = await userModel
+        .findById(newItem._id)
+      res.send(saved);
+    } catch (err) {
+      res.status(400).send({ message: err.message });
+    }
+  });
 
 router.put("/:id", async function (req, res, next) {
   try {
